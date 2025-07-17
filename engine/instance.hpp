d27b03648f5625c08df0e8bd42c65ed5a7fc1726
octaviovaloni <octaviosvaloni@gmail.com>
2025-07-17 05:05:01 -0300

    Deleted unecessary functions an instance.hpp

--- engine/instance.hpp
+++ engine/instance.hpp
@@ -11,8 +11,6 @@ namespace engine::types
         virtual ~Instance() = default;
         Instance();
 
-        std::vector<std::function<void()>> logic_update_connections;
-        std::vector<std::function<void()>> physics_update_connections;
         bool destroyed = false;
         bool able_to_render = true;
         bool able_to_update = true;
@@ -22,12 +20,6 @@ namespace engine::types
         {
             this->destroyed = true;
         }
-
-        void ConnectToPhysicsUpdate(const std::function<void()>& func);
-        void ConnectToLogicUpdate(const std::function<void()>& func);
-        void FireLogicUpdateConnections();
-        void FirePhysicsUpdateConnections();
-
 
         virtual void PhysicsUpdate(float delta);
         virtual void LogicUpdate(float delta);
